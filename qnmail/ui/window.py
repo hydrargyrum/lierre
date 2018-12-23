03946549aefbd6e9d2181a412c297a84ea12d406
hydrargyrum <dev@indigo.re>
2018-12-23 13:37:59 +0100

    display tags list

--- qnmail/ui/window.py
+++ qnmail/ui/window.py
@@ -1,11 +1,35 @@
 
+from hashlib import sha1
+
 from PyQt5.QtWidgets import (
     QMainWindow, QWidget, QApplication,
 )
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QBrush, QColor
 
 from .threadslist import threads_to_model
 from .threads_window_ui import Ui_Form
 from .threadview import ThreadWidget
+
+
+def all_tags_to_model(db):
+    mdl = QStandardItemModel()
+    mdl.setHorizontalHeaderLabels(['Tag'])
+
+    tags = db.get_all_tags()
+    tags = sorted(tags, key=str.lower)
+    for tag in tags:
+        item = QStandardItem(tag)
+
+        r, g, b = sha1(tag.encode('utf-8')).digest()[:3]
+        item.setBackground(QBrush(QColor(r, g, b)))
+        if r + g + b < 128 * 3:
+            item.setForeground(QBrush(QColor('white')))
+        else:
+            item.setForeground(QBrush(QColor('black')))
+
+        mdl.appendRow(item)
+
+    return mdl
 
 
 def get_thread_by_id(db, id):
@@ -44,6 +68,9 @@ class Window(QMainWindow):
         self.centralWidget().searchButton.clicked.connect(self.doSearch)
 
         self.setWindowTitle(self.tr('QNMail'))
+
+        app = QApplication.instance()
+        self.centralWidget().tagsView.setModel(all_tags_to_model(app.db))
 
     def doSearch(self):
         app = QApplication.instance()
