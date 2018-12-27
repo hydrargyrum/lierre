4ba580d6cc435f0588b3f3f8dad365d37b210e81
hydrargyrum <dev@indigo.re>
2018-12-27 22:01:32 +0100

    threads list: display last update date
    TODO: use better formatting, for example:
    - just the time when same day
    - relative formatting like "3 hours ago"

--- qnmail/ui/threadslist.py
+++ qnmail/ui/threadslist.py
@@ -1,16 +1,22 @@
 
+import datetime
+
 from PyQt5.QtWidgets import QTreeView
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
 
 def threads_to_model(threads):
     mdl = QStandardItemModel()
-    mdl.setHorizontalHeaderLabels(['ID', 'Subject'])
+    mdl.setHorizontalHeaderLabels(['ID', 'Subject', 'Last update'])
 
     for thr in threads:
         row = []
         row.append(QStandardItem(thr.get_thread_id()))
         row.append(QStandardItem(thr.get_subject()))
+
+        last_update = thr.get_newest_date()
+        last_update = datetime.datetime.fromtimestamp(last_update)
+        row.append(QStandardItem(last_update.strftime('%c')))
 
         for item in row:
             item.setEditable(False)
