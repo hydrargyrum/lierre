6ad1ae49760e02965da54cdcf812ef9bd370b74b
hydrargyrum <dev@indigo.re>
2019-02-11 23:26:59 +0100

    __main__: add a main() function, usable in setup.cfg entry_points

--- lierre/__main__.py
+++ lierre/__main__.py
@@ -7,14 +7,19 @@ import sys
 from .app import Application
 from .ui.window import Window
 
-if sys.excepthook is sys.__excepthook__:
-    sys.excepthook = lambda *args: sys.__excepthook__(*args)
+
+def main():
+    if sys.excepthook is sys.__excepthook__:
+        sys.excepthook = lambda *args: sys.__excepthook__(*args)
+
+    logging.basicConfig()
+    locale.setlocale(locale.LC_ALL, '')
+
+    app = Application(sys.argv)
+    win = Window()
+    win.show()
+    app.exec()
 
 
-logging.basicConfig()
-locale.setlocale(locale.LC_ALL, '')
-app = Application(sys.argv)
-win = Window()
-win.show()
-app.exec()
-
+if __name__ == '__main__':
+    main()
