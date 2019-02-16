cd88b6e5547b807c550640eaabfc87fed6f1d632
hydrargyrum <dev@indigo.re>
2019-02-16 17:38:41 +0100

    app: write config on quit

--- lierre/app.py
+++ lierre/app.py
@@ -1,8 +1,9 @@
 
+from PyQt5.QtCore import pyqtSlot as Slot
 from PyQt5.QtWidgets import QApplication
 
 from . import plugin_manager
-from .config import read_config
+from .config import read_config, write_config
 
 
 class Application(QApplication):
@@ -11,3 +12,8 @@ class Application(QApplication):
         read_config()
         plugin_manager.init()
 
+        self.aboutToQuit.connect(self._on_quit)
+
+    @Slot()
+    def _on_quit(self):
+        write_config()
