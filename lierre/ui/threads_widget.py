f2ab836b26efdcdefe3279d3463b7f60b3cefea1
hydrargyrum <dev@indigo.re>
2019-02-27 23:53:50 +0100

    ui threads widget: add a toolbar for archiving/deleting/setting tags

--- lierre/ui/threads_widget.py
+++ lierre/ui/threads_widget.py
@@ -1,14 +1,16 @@
 
 
-from PyQt5.QtWidgets import QWidget, QStyledItemDelegate, QStyle
+from PyQt5.QtWidgets import QWidget, QStyledItemDelegate, QStyle, QToolBar
 from PyQt5.QtGui import QFontMetrics, QPen
 from PyQt5.QtCore import (
     pyqtSignal as Signal, pyqtSlot as Slot, Qt, QSize,
 )
-from lierre.utils.db_ops import open_db
+from lierre.utils.db_ops import open_db, open_db_rw, get_thread_by_id
 from lierre.config import CONFIG
+from lierre.change_watcher import WATCHER
 
 from .models import ThreadListModel, TagsListModel, MaildirFlags, tag_to_colors
+from .tag_editor import TagEditor
 from .ui_loader import load_ui_class
 
 
@@ -87,6 +89,15 @@ class ThreadSubjectDelegate(ThreadUnreadDelegate):
         return QSize(x, y)
 
 
+def iter_thread_messages(thread):
+    def recurse_list(it):
+        for msg in it:
+            yield msg
+            yield from recurse_list(msg.get_replies())
+
+    yield from recurse_list(thread.get_toplevel_messages())
+
+
 class ThreadsWidget(QWidget, Ui_Form):
     def __init__(self, *args, **kwargs):
         super(ThreadsWidget, self).__init__(*args, **kwargs)
@@ -102,6 +113,7 @@ class ThreadsWidget(QWidget, Ui_Form):
         self.threadsView.setItemDelegate(ThreadUnreadDelegate())
         self.threadsView.setItemDelegateForColumn(0, ThreadSubjectDelegate())
         self.threadsView.setDragEnabled(True)
+        self.threadsView.selectionModel().selectionChanged.connect(self.updateToolbarState)
 
         for col, sz in enumerate(CONFIG.get('ui', 'threads_view', 'columns', default=[])):
             self.threadsView.setColumnWidth(col, sz)
@@ -112,6 +124,25 @@ class ThreadsWidget(QWidget, Ui_Form):
         self.searchButton.clicked.connect(self.doSearch)
 
         self.splitter.setStretchFactor(1, 1)
+
+        self.setupToolbar()
+
+    def setupToolbar(self):
+        tb = QToolBar()
+        self.verticalLayout.insertWidget(0, tb)
+
+        tb.addAction(self.actionArchive)
+        self.actionArchive.triggered.connect(self._archiveThreads)
+
+        tb.addAction(self.actionTags)
+        self.actionTags.triggered.connect(self.openTagEditor)
+
+        tb.addSeparator()
+
+        tb.addAction(self.actionDelete)
+        self.actionDelete.triggered.connect(self._deleteSelectedThreads)
+
+        self.updateToolbarState()
 
     def _openThread(self, qidx):
         tid = qidx.data(ThreadListModel.ThreadIdRole)
@@ -137,3 +168,88 @@ class ThreadsWidget(QWidget, Ui_Form):
         CONFIG.set('ui', 'threads_view', 'columns', cols)
         for col in range(self.threadsView.model().columnCount()):
             cols.append(self.threadsView.columnWidth(col))
+
+    @Slot()
+    def updateToolbarState(self):
+        nb_selected = len([
+            qidx for qidx in self.threadsView.selectedIndexes()
+            if qidx.column() == 0
+        ])
+
+        self.actionTags.setEnabled(bool(nb_selected))
+        self.actionDelete.setEnabled(bool(nb_selected))
+        self.actionArchive.setEnabled(bool(nb_selected))
+
+    def _getSelectedThreads(self):
+        return [
+            qidx.data(ThreadListModel.ThreadIdRole)
+            for qidx in self.threadsView.selectedIndexes()
+            if qidx.column() == 0
+        ]
+
+    def _iter_selected_threads(self, db):
+        thread_ids = self._getSelectedThreads()
+        return (get_thread_by_id(db, thread_id) for thread_id in thread_ids)
+
+    @Slot()
+    def _deleteSelectedThreads(self):
+        with open_db_rw() as db:
+            for thread in self._iter_selected_threads(db):
+                for msg in iter_thread_messages(thread):
+                    msg.add_tag('deleted', True)
+                    WATCHER.tagMailAdded.emit('deleted', msg.get_message_id())
+
+    @Slot()
+    def _archiveThreads(self):
+        with open_db_rw() as db:
+            for thread in self._iter_selected_threads(db):
+                for msg in iter_thread_messages(thread):
+                    msg.remove_tag('inbox')
+                    WATCHER.tagMailRemoved.emit('inbox', msg.get_message_id())
+
+    @Slot()
+    def openTagEditor(self):
+        with open_db() as db:
+            union_tags = set()
+            common_tags = None
+            for thread in self._iter_selected_threads(db):
+                for msg in iter_thread_messages(thread):
+                    msg_tags = set(msg.get_tags())
+                    union_tags |= msg_tags
+                    if common_tags is None:
+                        common_tags = msg_tags
+                    else:
+                        common_tags &= msg_tags
+
+            union_tags -= common_tags
+
+        w = TagEditor(parent=self)
+        w.setCheckedTags(common_tags, union_tags)
+
+        if not w.exec_():
+            return
+
+        checked, partially = w.checkedTags()
+
+        with open_db_rw() as db:
+            for thread in self._iter_selected_threads(db):
+                for msg in iter_thread_messages(thread):
+                    msg_tags = set(msg.get_tags())
+                    to_remove = msg_tags - (checked | partially)  # unchecked tags
+                    to_add = checked - msg_tags  # newly checked tags
+
+                    msg.freeze()
+
+                    for tag in to_remove:
+                        msg.remove_tag(tag)
+                    for tag in to_add:
+                        msg.add_tag(tag)
+
+                    msg.thaw()
+                    msg.tags_to_maildir_flags()
+
+                    for tag in to_remove:
+                        WATCHER.tagMailRemoved.emit(tag, msg.get_message_id())
+                    for tag in to_add:
+                        WATCHER.tagMailAdded.emit(tag, msg.get_message_id())
+
