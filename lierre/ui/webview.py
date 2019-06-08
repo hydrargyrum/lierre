fd04cddd8dbdabe10eb0587fbb284394ab895b42
hydrargyrum <dev@indigo.re>
2019-06-08 13:47:37 +0200

    ui/webview: implement alternative WebView with QTextBrowser
    Pros:
    * codebase is multiple orders of magnitude smaller than full-fledged
    google-chromium-based webengine
    * smaller footprint
    * widget size fit-to-contents is more reliable
    * javascript isn't supported, which is safer by default
    * network isn't implemented, which is safer by default
    
    Cons:
    * supports a contrived HTML/CSS subset
    (https://doc.qt.io/qt-5/richtext-html-subset.html)

--- lierre/ui/webview.py
+++ lierre/ui/webview.py
@@ -1,8 +1,11 @@
 
 import logging
 
-from PyQt5.QtCore import QSizeF, pyqtSlot as Slot, QBuffer, QByteArray
+from PyQt5.QtCore import (
+    QSizeF, pyqtSlot as Slot, QBuffer, QByteArray, QVariant, QTimer,
+)
 from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtWidgets import QTextBrowser
 from PyQt5.QtWebEngineCore import (
     QWebEngineUrlRequestInterceptor, QWebEngineUrlRequestInfo,
     QWebEngineUrlSchemeHandler,
@@ -13,6 +16,8 @@ from PyQt5.QtWebEngineWidgets import (
 
 
 LOGGER = logging.getLogger(__name__)
+ENGINE_LOGGER = LOGGER.getChild('webengine')
+TEXT_LOGGER = LOGGER.getChild('textbrowser')
 
 
 class CidSchemeHandler(QWebEngineUrlSchemeHandler):
@@ -34,6 +39,7 @@ class CidSchemeHandler(QWebEngineUrlSchemeHandler):
 
     def requestStarted(self, req):
         if req.requestMethod() != b'GET':
+            ENGINE_LOGGER.debug('prevented non-GET to cid: resource')
             return req.fail(req.RequestFailed)
 
         path = req.requestUrl().path()
@@ -62,17 +68,17 @@ class Interceptor(QWebEngineUrlRequestInterceptor):
         if method == 'GET' and req.navigationType() == QWebEngineUrlRequestInfo.NavigationTypeLink:
             # warning: clicks must be accepted else acceptNavigationRequest() won't be called
             # and worse: the page will be blanked due to what looks like a qt bug
-            LOGGER.debug('accepted click request %s %s (navigation: %s, resource: %s)', method, url, req.navigationType(), req.resourceType())
+            ENGINE_LOGGER.debug('accepted click request %s %s (navigation: %s, resource: %s)', method, url, req.navigationType(), req.resourceType())
             return
 
         if method != 'GET' or req.resourceType() not in self.accepted_types:
             req.block(True)
-            LOGGER.debug('blocked request %s %s (navigation: %s, resource: %s)', method, url, req.navigationType(), req.resourceType())
+            ENGINE_LOGGER.debug('blocked request %s %s (navigation: %s, resource: %s)', method, url, req.navigationType(), req.resourceType())
             return
 
         if not url.startswith('data:') and not url.startswith('cid:'):
             req.block(True)
-            LOGGER.debug('blocked request %s %s (navigation: %s, resource: %s)', method, url, req.navigationType(), req.resourceType())
+            ENGINE_LOGGER.debug('blocked request %s %s (navigation: %s, resource: %s)', method, url, req.navigationType(), req.resourceType())
             return
 
 
@@ -90,9 +96,9 @@ class WebEnginePage(QWebEnginePage):
         return False
 
 
-class WebView(QWebEngineView):
+class WebEngineView(QWebEngineView):
     def __init__(self, *args, **kwargs):
-        super(WebView, self).__init__(*args, **kwargs)
+        super(WebEngineView, self).__init__(*args, **kwargs)
 
         self.interceptor = Interceptor(self)
         self.cid_handler = CidSchemeHandler(self)
@@ -121,3 +127,62 @@ class WebView(QWebEngineView):
 
     def setMessage(self, pymessage):
         self.cid_handler.setMessage(pymessage)
+
+
+class TextBrowserView(QTextBrowser):
+    def __init__(self, *args, **kwargs):
+        super(TextBrowserView, self).__init__(*args, **kwargs)
+        self.setOpenExternalLinks(True)
+
+        self.pymessage = None
+
+        self._resizeTimer = QTimer(parent=self)
+        self._resizeTimer.setSingleShot(True)
+        self._resizeTimer.setInterval(0)
+        self._resizeTimer.timeout.connect(self.updateSize)
+
+    def setMessage(self, pymessage):
+        self.pymessage = pymessage
+
+        self.parts = {}
+        for part in self.pymessage.walk():
+            cid = part.get('Content-ID')
+            if not cid:
+                continue
+
+            self.parts[cid[1:-1]] = part
+
+    def loadResource(self, type, qurl):
+        url = bytes(qurl.toEncoded()).decode('ascii')
+        if qurl.scheme() != 'cid':
+            TEXT_LOGGER.debug('preventing %r to be loaded', url)
+            return QVariant()
+
+        TEXT_LOGGER.debug('loading %r from parts', url)
+        path = qurl.path()
+        try:
+            part = self.parts[path]
+        except KeyError:
+            TEXT_LOGGER.debug('could not find part %r', path)
+            return QVariant()
+
+        self._resizeTimer.start()
+        # XXX decode=True: decodes message ASCII to bytes
+        return QVariant(QByteArray(part.get_payload(decode=True)))
+
+    @Slot(str)
+    def setHtml(self, s):
+        self._resizeTimer.start()
+        return super(TextBrowserView, self).setHtml(s)
+
+    def sizeHint(self):
+        sz = self.document().size().toSize()
+        sz.setHeight(20 + sz.height())
+        return sz
+
+    @Slot()
+    def updateSize(self):
+        self.updateGeometry()
+
+
+WebView = TextBrowserView
