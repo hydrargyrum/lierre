4ee6eb51f2c36fcf107f7f1d0ed67faadf2a0bc5
hydrargyrum <dev@indigo.re>
2019-06-15 16:37:59 +0200

    plugins: add thread_assign_dir builtin filter plugin
    This plugin will move all messages from INBOX into the dir where its
    thread's toplevel message is. With this, a whole thread will be in the
    same maildir folder.

---
+++ lierre/builtins/filters/thread_assign_dir.py
@@ -0,0 +1,68 @@
+
+from time import time
+import mailbox
+from pathlib import Path
+
+from lierre.utils.db_ops import open_db_rw, get_db_path, get_thread_by_id
+from lierre.utils.box_ops import move_to_mailbox, box_path_from_msg
+
+from ..fetchers.base import Plugin, Job
+
+
+THRESHOLD = 24 * 60 * 60
+
+
+class ThreadToDirPlugin(Plugin):
+    def get_config(self):
+        return self.config
+
+    def set_config(self, config):
+        self.config = config
+
+    def create_job(self):
+        return MoveJob()
+
+
+class MoveJob(Job):
+    def start(self):
+        now = time()
+        mailroot = get_db_path()
+        box = mailbox.Maildir(mailroot)
+
+        # TODO: folder:"" and thread:{not folder:""}?
+        with open_db_rw() as db:
+            for msgname in box.iterkeys():
+                msg_path = Path(mailroot).joinpath(box._lookup(msgname))
+
+                if now - msg_path.stat().st_mtime >= THRESHOLD:
+                    continue
+
+                if 'deleted' in set(db.find_message_by_filename(str(msg_path)).get_tags()):
+                    continue
+
+                try_place_in_folder(db, msg_path)
+
+        self.finished.emit(0)
+
+
+def try_place_in_folder(db, msg_path):
+    msg = db.find_message_by_filename(str(msg_path))
+
+    thread = get_thread_by_id(db, msg.get_thread_id())
+    for toplevel in thread.get_toplevel_messages():
+        # TODO: in what case multiple toplevel messages?
+        # TODO: multiple filenames for toplevel message?
+        top_path = Path(toplevel.get_filename())
+
+        if msg_path.parent == top_path.parent:
+            continue
+
+        if box_path_from_msg(msg_path) == box_path_from_msg(top_path):
+            # maildir: one may be in 'new' and the other in 'cur'
+            continue
+
+        new_path = move_to_mailbox(msg_path, box_path_from_msg(top_path))
+
+        db.add_message(str(new_path))
+        db.remove_message(str(msg_path))
+        break
