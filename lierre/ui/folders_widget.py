53b1f9fe6ce53318a8e9d6f0fa6bdc50d3638b4b
hydrargyrum <dev@indigo.re>
2019-06-15 15:07:04 +0200

    ui: add a FoldersWidget class and introduce utils.box_ops
    FoldersWidget is unused in the app, mostly for debugging for now.
    
    lierre.utils.box_ops will be useful for manipulating maildir folders:
    list them, move messages, etc.

---
+++ lierre/ui/folders_widget.py
@@ -0,0 +1,220 @@
+
+from itertools import chain
+import json
+from logging import getLogger
+from pathlib import Path
+
+from PyQt5.QtCore import (
+    QModelIndex, QVariant, Qt, QMimeData, QSortFilterProxyModel,
+)
+from PyQt5.QtCore import pyqtSlot as Slot, pyqtSignal as Signal
+from PyQt5.QtWidgets import QWidget
+from lierre.utils.db_ops import open_db
+from lierre.utils.box_ops import list_folders, move_to_mailbox
+from lierre.utils.date import short_datetime
+from lierre.utils.addresses import get_sender
+
+from .ui_loader import load_ui_class
+from .models import BasicTreeModel, BasicListModel, register_role
+
+
+LOGGER = getLogger(__name__)
+
+Ui_Form = load_ui_class('folders_widget', 'Ui_Form')
+
+
+class FoldersModel(BasicTreeModel):
+    FolderFilenameRole = register_role()
+    FolderObjectRole = register_role()
+
+    columns = (
+        ('Box', 'box'),
+    )
+
+    messageMoved = Signal()
+
+    def __init__(self, *args, **kwargs):
+        super(FoldersModel, self).__init__(*args, **kwargs)
+        self.refresh()
+
+    def refresh(self):
+        tree = {}
+        objs = {}
+        for folder in sorted(list_folders()):
+            if folder.parts:
+                objs[folder.parts] = {
+                    'key': folder.parts,
+                    'box': folder.parts[-1],
+                    'obj': folder,
+                }
+                tree.setdefault(folder.parts[:-1], []).append(folder.parts)
+            else:  # INBOX
+                objs[folder.parts] = {
+                    'key': folder.parts,
+                    'box': 'INBOX',
+                    'obj': folder,
+                }
+                tree.setdefault(None, []).append(folder.parts)
+
+        self._setTree(tree, objs)
+
+    def data(self, qidx, role=Qt.DisplayRole):
+        item = qidx.internalPointer()
+        if item is None:
+            return QVariant()
+
+        if role == self.FolderFilenameRole:
+            return QVariant(item['obj'].dir)
+        elif role == self.FolderObjectRole:
+            return QVariant(item['obj'])
+        elif role == Qt.DisplayRole:
+            name = self.columns[qidx.column()][1]
+            data = item[name]
+            return QVariant(data)
+
+        return QVariant()
+
+    def flags(self, qidx):
+        flags = super(FoldersModel, self).flags(qidx)
+        if flags & Qt.ItemIsEnabled:
+            flags |= Qt.ItemIsDropEnabled
+        return flags
+
+    def supportedDropActions(self):
+        return Qt.MoveAction
+
+    def mimeTypes(self):
+        return ['text/x-lierre-messages']
+
+    def dropMimeData(self, mime, action, row, column, parent_qidx):
+        data = bytes(mime.data('text/x-lierre-messages')).decode('ascii')
+        if not data:
+            return False
+
+        try:
+            paths = json.loads(data)
+        except ValueError:
+            LOGGER.warning('failed to decode dropped data')
+            return False
+
+        if row == column == -1:
+            qidx = parent_qidx.siblingAtColumn(0)
+        else:
+            qidx = parent_qidx.child(row, 0)
+
+        folder = qidx.data(self.FolderObjectRole)
+        for msg_path in paths:
+            move_to_mailbox(Path(msg_path), folder.path)
+            self.messageMoved.emit()
+
+        return True
+
+
+class FolderMessagesModel(BasicListModel):
+    MessageFileRole = register_role()
+
+    columns = (
+        ('Date', 'date'),
+        ('Author', 'sender'),
+        ('Subject', 'subject'),
+    )
+
+    path = None
+
+    def setPath(self, path):
+        self.path = path
+        self.refresh()
+
+    @Slot()
+    def refresh(self):
+        objs = []
+        with open_db() as db:
+            sub_iter = chain(
+                self.path.joinpath('new').iterdir(),
+                self.path.joinpath('cur').iterdir(),
+            )
+            for msg_path in sub_iter:
+                msg = db.find_message_by_filename(str(msg_path))
+                if msg is None:
+                    continue
+
+                objs.append({
+                    'date': msg.get_date(),
+                    'tags': frozenset(msg.get_tags()),
+                    'subject': msg.get_header('subject'),
+                    'sender': msg.get_header('From'),
+                    'message_id': msg.get_message_id(),
+                    'thread_id': msg.get_thread_id(),
+                    'path': msg_path,
+                })
+
+        self._setObjs(objs)
+
+    def data(self, qidx, role=Qt.DisplayRole):
+        item = qidx.internalPointer()
+        if item is None:
+            return QVariant()
+
+        if role == self.MessageFileRole:
+            return QVariant(str(item['path']))
+        elif role == Qt.DisplayRole:
+            name = self.columns[qidx.column()][1]
+            data = item[name]
+
+            if name == 'sender':
+                data = get_sender(data)
+            elif name == 'date':
+                data = short_datetime(data)
+
+            return QVariant(data)
+
+        return QVariant()
+
+    def supportedDragActions(self):
+        return Qt.MoveAction
+
+    def flags(self, qidx):
+        flags = super(FolderMessagesModel, self).flags(qidx)
+        if flags & Qt.ItemIsEnabled:
+            flags |= Qt.ItemIsDragEnabled
+        return flags
+
+    def mimeTypes(self):
+        return ['text/x-lierre-messages']
+
+    def mimeData(self, qidxs):
+        paths = [qidx.data(self.MessageFileRole) for qidx in qidxs if qidx.column() == 0]
+        mime = QMimeData()
+        mime.setData('text/x-lierre-messages', json.dumps(paths).encode('ascii'))
+        return mime
+
+
+class MessageSorter(QSortFilterProxyModel):
+    def lessThan(self, a, b):
+        assert a.column() == b.column()
+
+        if a.column() == 0:
+            return a.internalPointer()['date'] < b.internalPointer()['date']
+        return super(MessageSorter, self).lessThan(a, b)
+
+
+class FoldersWidget(QWidget, Ui_Form):
+    def __init__(self, *args, **kwargs):
+        super(FoldersWidget, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+        self.folderTree.setModel(FoldersModel(parent=self))
+        self.folderTree.expandAll()
+        self.folderTree.activated.connect(self._folderActivated)
+
+        self.messageModel = FolderMessagesModel(parent=self)
+        sorter = MessageSorter(parent=self)
+        sorter.setSourceModel(self.messageModel)
+        self.messageList.setModel(sorter)
+
+        self.folderTree.model().messageMoved.connect(self.messageModel.refresh)
+
+    @Slot(QModelIndex)
+    def _folderActivated(self, qidx):
+        dir = qidx.data(FoldersModel.FolderFilenameRole)
+        self.messageModel.setPath(Path(dir))
---
+++ lierre/utils/box_ops.py
@@ -0,0 +1,158 @@
+
+from base64 import b64encode, b64decode
+from collections import namedtuple
+from logging import getLogger
+import mailbox
+from pathlib import Path
+import re
+from time import time
+
+from .db_ops import get_db_path, open_db_rw
+
+
+LOGGER = getLogger(__name__)
+
+
+def decode_maildir_name(name: str) -> str:
+    # what a great encoding!
+    def pad(b):
+        if len(b) % 4:
+            b += '=' * (4 - (len(b) % 4))
+        return b
+
+    def replace(mtc):
+        seq = mtc.group(1)
+        if seq == '':
+            return '&'
+        seq = pad(seq)
+        return b64decode(seq, '+,').decode('utf-16-be')
+
+    if isinstance(name, bytes):
+        name = name.decode('ascii')
+
+    name = re.sub(r'&([A-Za-z0-9+,]*)-', replace, name)
+    return name
+
+
+def encode_maildir_name(s: str) -> str:
+    ret = bytearray()
+    amp = ord('&')
+    reserved = ord('.'), ord('/')
+
+    s = s.encode('utf-16-be')
+    for hi, lo in zip(s[::2], s[1::2]):
+        if hi == 0:
+            if lo == amp:
+                ret.extend(b'&-')
+                continue
+            elif lo <= 127 and lo not in reserved:
+                ret.append(lo)
+                continue
+
+        ret += b'&'
+        ret.extend(b64encode(bytes([hi, lo]), b'+,').rstrip(b'='))
+        ret += b'-'
+
+    return ret.decode('ascii')
+
+
+def subpath_to_maildir_name(subpath: str) -> str:
+    path = '.'.join(encode_maildir_name(part) for part in subpath.split('/'))
+    return '.%s' % path
+
+
+def subpath_from_maildir_name(name: str) -> str:
+    name = name.lstrip('.')
+    return '/'.join(decode_maildir_name(part) for part in name.split('.'))
+
+
+def test_maildir_encodings():
+    assert encode_maildir_name('Rés.umé') == 'R&AOk-s&AC4-um&AOk-'
+    assert 'Rés.umé' == decode_maildir_name('R&AOk-s&AC4-um&AOk-')
+
+    assert subpath_to_maildir_name('foo.bar/qux') == '.foo&AC4-bar.qux'
+    assert 'foo.bar/qux' == subpath_from_maildir_name('.foo&AC4-bar.qux')
+
+
+def get_box_path(box_name: str) -> Path:
+    path = Path(get_db_path())
+    return path.joinpath(subpath_from_maildir_name(box_name))
+
+
+def box_path_from_msg(msg_path) -> Path:
+    msg_path = Path(msg_path)
+    return msg_path.parent.parent
+
+
+class Folder(namedtuple('Folder', ('path', 'parts'))):
+    @property
+    def dir(self):
+        return str(self.path)
+
+    @property
+    def encoded_name(self):
+        return self.path.name
+
+    @property
+    def basename(self):
+        return self.parts[-1]
+
+
+def list_folders():
+    root = Path(get_db_path())
+
+    yield Folder(root, ())
+
+    for sub in root.iterdir():
+        if not (sub.is_dir() and sub.joinpath('cur').exists()):
+            continue
+
+        yield Folder(
+            sub,
+            tuple(decode_maildir_name(part) for part in sub.name[1:].split('.'))
+        )
+
+
+TMP_CLEAN_THRESHOLD_SECS = 36 * 60 * 60  # 36 hours
+
+
+def clean_tmp_box(box):
+    box = Path(box)
+
+    now = time()
+    for sub in box.joinpath('tmp').iterdir():
+        if sub.is_file() and now - sub.stat().st_mtime > TMP_CLEAN_THRESHOLD_SECS:
+            sub.unlink()
+
+
+def move_to_mailbox(src_path, target_box_path) -> Path:
+    LOGGER.debug('moving %r to %r', src_path, target_box_path)
+    src_path = Path(src_path)
+    target_box_path = Path(target_box_path)
+
+    box = mailbox.Maildir(str(target_box_path))
+
+    with src_path.open('rb') as src_fd:
+        new_key = box.add(src_fd)
+    dest_path = target_box_path.joinpath(box._lookup(new_key))
+
+    with open_db_rw() as db:
+        db.add_message(str(dest_path))
+        db.remove_message(str(src_path))
+    src_path.unlink()
+
+    return dest_path
+
+
+def change_flags(src_path, to_add=None, to_remove=None):
+    to_add = set(to_add or [])
+    to_remove = set(to_remove or [])
+    src_path = Path(src_path)
+
+    name, _, flags = src_path.name.partition(',')
+    flags = set(flags)
+    flags |= to_add
+    flags -= to_remove
+
+    dest_path = src_path.with_name('%s,%s' % (name, ''.join(flags)))
+    src_path.rename(dest_path)
