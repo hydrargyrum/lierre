c2e4159e8ced6117a774de9817bd93ed8beec358
hydrargyrum <dev@indigo.re>
2019-07-30 23:57:57 +0200

    ui.ui_loader: support .ui files not in lierre.ui package (for plugins)
    This is useful for plugins to have their configuration .ui files.

--- lierre/ui/ui_loader.py
+++ lierre/ui/ui_loader.py
@@ -5,11 +5,13 @@ from pathlib import Path
 from PyQt5.uic import loadUiType
 
 
-def load_ui_class(module_name, class_name):
+def load_ui_class(module_name, class_name, package='lierre.ui'):
     try:
-        mod = import_module('.%s_ui' % module_name, 'lierre.ui')
+        mod = import_module(f'.{module_name}_ui', package)
     except ImportError:
-        path = Path(__file__).with_name('%s.ui' % module_name)
+        pkg_parts = package.split('.')
+        folder = Path(__file__).parent.parent.joinpath(*pkg_parts[1:])
+        path = folder.joinpath(f'{module_name}.ui')
         return loadUiType(str(path))[0]
     else:
         return getattr(mod, class_name)
