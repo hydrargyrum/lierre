f86137b56a117ce438f38ebfc46d637ce350e7c9
hydrargyrum <dev@indigo.re>
2019-08-05 23:02:33 +0200

    ui: add identities conf, read-only for now

--- lierre/ui/options_conf.py
+++ lierre/ui/options_conf.py
@@ -1,6 +1,9 @@
 # this project is licensed under the WTFPLv2, see COPYING.wtfpl for details
 
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import Qt, pyqtSlot as Slot
+from PyQt5.QtWidgets import QDialog, QListWidgetItem
+from lierre.sending import get_identities
+from lierre.plugin_manager import PLUGINS
 
 from .ui_loader import load_ui_class
 
@@ -15,6 +18,7 @@ class OptionsConf(Ui_Dialog, QDialog):
 
         self.buttonBox.rejected.connect(self.reject)
 
+        # plugins
         self.fetchersConf.setPluginKind('fetchers')
         self.filtersConf.setPluginKind('filters')
         self.sendersConf.setPluginKind('senders')
@@ -22,3 +26,20 @@ class OptionsConf(Ui_Dialog, QDialog):
 
         for widget in (self.fetchersConf, self.filtersConf, self.sendersConf, self.miscConf):
             self.finished.connect(widget.updateConfig)
+
+        # identities
+        identities = get_identities()
+        for idt in identities.values():
+            qitem = QListWidgetItem(idt.name)
+            qitem.setData(Qt.UserRole, idt)
+            self.identitiesList.addItem(qitem)
+
+        for key in PLUGINS['senders'].iter_enabled_plugins():
+            self.senderCombo.addItem(key)
+
+    @Slot(QListWidgetItem, QListWidgetItem)
+    def on_identitiesList_currentItemChanged(self, qitem, _):
+        idt = qitem.data(Qt.UserRole)
+        self.fullNameEdit.setText(idt.name)
+        self.emailEdit.setText(idt.address)
+        self.senderCombo.setCurrentText(idt.sender_plugin)
